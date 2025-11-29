cbefa59551bc300cb252e55555b7ea7ce42c1485
Twin Edo <twinedo.dev@gmail.com>
2025-11-30 02:21:43 +0700

    add zustandStorage for handling local storage as easy as AsyncStorage

---
+++ utils/storage.ts
@@ -0,0 +1,17 @@
+import { createMMKV } from 'react-native-mmkv'
+import { StateStorage } from 'zustand/middleware'
+
+const storage = createMMKV()
+
+export const zustandStorage: StateStorage = {
+  setItem: (name, value) => {
+    return storage.set(name, value)
+  },
+  getItem: (name) => {
+    const value = storage.getString(name)
+    return value ?? null
+  },
+  removeItem: (name) => {
+    return storage.remove(name)
+  },
+}
